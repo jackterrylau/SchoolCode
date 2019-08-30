9d3c8cd464b159db5ec28dad06e7718359b06ad5
jackterrylau <jackterrylau@yahoo.com.tw>
2019-08-31 05:16:03 +0800

    add funcWithRequiredArgus.js

---
+++ Javascript/funcWithRequiredArgs.js
@@ -0,0 +1,44 @@
+//Implement Javascript function with required parameters.
+
+// Approach 1. Check undefined value method.(簡查參數法)
+function testMethod1(x,y) {
+    if (x===undefined && y===undefined) {throw new Error("Parameter x and y is required")}
+    if (x===undefined) {throw new Error("Parameter x is required")}
+    if (y===undefined) {throw new Error("Parameter y is required")}
+
+    return x+y
+}
+
+// Approach 2. Parameter with exception method.(參數預設值強制丟出異常法)
+function throwRequiredException() {
+    throw new Error("Missing parameter") 
+}
+
+function testMethod2(a=throwRequiredException()) {
+    return a*a;
+}
+
+// test function
+
+try {
+    testMethod1(1)
+} catch (e) { 
+    console.log("testMethod1(1) : " + e.message)
+}
+
+try {
+    testMethod1()
+} catch (e) { 
+    console.log("testMethod1() : " + e.message)
+}
+
+console.log("testMethod1(1,2) = " + testMethod1(1,2))
+
+try {
+    testMethod2()
+} catch (e) { 
+    console.log("testMethod2() : " + e.message)
+}
+
+console.log("testMethod2(3) = " + testMethod2(3))
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Read Me
 # 放我的Practice Sample Code 的地方
+
+Javascript
+ - funcWithRequiredArgs.js : 測試兩種函式強迫傳入參數值的方法。
